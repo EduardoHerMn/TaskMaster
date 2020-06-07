728f855cfc1bcc473e2e48b60ca14aebc91a630d
Valter <valter.nunez@outlook.com>
2020-06-06 20:21:42 -0500

    Pick IMage Gallery

--- app/src/main/java/com/example/taskmaster2/TaskDetailActivity.kt
+++ app/src/main/java/com/example/taskmaster2/TaskDetailActivity.kt
@@ -1,7 +1,13 @@
 package com.example.taskmaster2
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.taskmaster2.R
 import kotlinx.android.synthetic.main.activity_task_detail.*
 
@@ -23,5 +29,70 @@ class TaskDetailActivity : AppCompatActivity() {
         detail_hora.text = hora
         detail_ubicacion.text = lugar
 
+
+        //IMAGE PICK BUTTON CLICK
+        imageView3.setOnClickListener{
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                    //PERMISSION DENIED
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    //show popup to request permission
+                    requestPermissions(permissions, PERMISION_CODE);
+                }
+                else {
+                    //permission already granted
+                    pickImageFromGallery();
+
+                }
+            }
+            else {
+                //Systen OS is <= Marshmallow
+                pickImageFromGallery();
+            }
+        }
+
+    }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+
+
+    }
+
+    companion object {
+        //image pick code
+        private val IMAGE_PICK_CODE = 1000;
+        //Permission code
+        private val PERMISION_CODE = 10001;
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            PERMISION_CODE -> {
+                if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //permissionn popup
+                    pickImageFromGallery();
+                }
+                else {
+                    //permissionfrom popup denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            imageView3.setImageURI(data?.data);
+
+        }
     }
 }
